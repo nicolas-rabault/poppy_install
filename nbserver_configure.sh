e76b398b83c256c43b3ade619e1912189484698e
daweeed <david.forgeron@gmail.com>
2015-03-25 19:24:12 +0100

    Creating deamon for ipython notebook

--- nbserver_configure.sh
+++ nbserver_configure.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
 
+# Create a new profile
 ipython profile create nbserver
+
+# Donwload and move ipython config
+wget https://raw.githubusercontent.com/Inmediats-Poppy/poppy_install/master/profile_nbserver/ipython_notebook_config.py
+cp /home/poppy/.ipython/profile_nbserver/ipython_notebook_config.py /home/poppy/.ipython/profile_nbserver/ipython_notebook_config.py.BAK
+cp ipython_notebook_config.py /home/poppy/.ipython/profile_nbserver/ipython_notebook_config.py
+
+# ipython notebook --profile=nbserver
+# Deamon to create
+wget https://raw.githubusercontent.com/Inmediats-Poppy/poppy_install/master/src/nbserver
+sudo cp nbserver /etc/init.d/nbserver
+sudo update-rc.d nbserver defaults
